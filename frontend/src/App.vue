e52b755529838a2acad3a4213f0f94d218c54fbb
Chen-Yu Yu <tobyyu007@gmail.com>
2023-05-20 19:13:18 -0700

    Add back map vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,87 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Map,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
 
+
+<template>
+  <div id="container">
+    <v-row no-gutters id="upperRow" style="height: 65vh; width: 100%;">
+      <v-col no-gutters id="map"
+        style="justify-content: center; align-items: center; display:grid; margin: 15px 5px 7.5px 15px; padding: 10px 10px 5px 10px">
+        <Map />
+      </v-col>
+      <v-col no-gutters id="toolbox" style="height: 20vh; justify-content: center; align-items: center; display:grid; margin: 15px 15px 7.5px 5px; padding: 10px 10px 5px 10px">
+        <toolbox />
+      </v-col>
+    </v-row>
+    <v-row no-gutters id="lowerRow" style="height: 35vh; width: 100%;">
+      <v-col no-gutters style="display:grid; margin: 10px 7.5px 15px 15px;"
+        id="stockRecommendation">
+        123
+      </v-col>
+      <v-col no-gutters id="wordcloud" style="height: 10vh; justify-content: center; align-items: center; margin: 10px 15px 15px 7.5px;">
+        456
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+html,
+body {
+  width: 100%;
+  height: 100%;
+  min-width: 1300px;
+  margin: 0;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  font: 15px -apple-system, BlinkMacSystemFont,
+    Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+    Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: flex;
+  background-color: #BDC0BA;
+}
+
+#map,
+#news-display,
+#stockRecommendation,
+#wordcloud,
+#toolbox {
+  -webkit-border-radius: 15px;
+  -moz-border-radius: 15px;
+  border-radius: 15px;
+  border: none;
+  background-color: #FFF;
+}
+
+.map {
+  grid-area: map;
+}
+
+.toolbox {
+  grid-area: toolbox;
+}
+
+.News-Recommendation {
+  grid-area: News-Recommendation;
+}
+
+.Wordcloud {
+  grid-area: Wordcloud;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import VueGoogleMaps from 'vue-google-maps-community-fork'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyDImDc1pPOI2VTYG8Pb0xPedM8TuYHvI8A',
+    },
+  })
+  .mount('#app')
