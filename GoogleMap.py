d4c01940ee7399fe7bf1028f898ed8b517762c38
顧元皓 <kuyuanhao@guyuanhaos-MacBook-Air.local>
2023-05-20 19:45:15 -0700

    create json

--- GoogleMap.py
+++ GoogleMap.py
@@ -1,15 +1,21 @@
 import googlemaps
 from datetime import datetime
 
-if __name__ == "__main__":
-
+def GetGeoCode(address):
     gmaps = googlemaps.Client(key='AIzaSyDImDc1pPOI2VTYG8Pb0xPedM8TuYHvI8A')
+    geocode_result = gmaps.geocode(address+'Davis, CA')
+    geocode_result_dict = {k: v for d in geocode_result for k, v in d.items()}
+    return geocode_result_dict['geometry']['location']
 
+if __name__ == "__main__":
+    gmaps = googlemaps.Client(key='AIzaSyDImDc1pPOI2VTYG8Pb0xPedM8TuYHvI8A')
+    
     # Geocoding an address
     addresses = ['1440 Wake Forest Drive, Davis, CA','250 W Quad, Davis, CA ']
     geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
     geocode_result_dict = {k: v for d in geocode_result for k, v in d.items()}
     print(geocode_result_dict['geometry']['location'])
+    '''
     # Look up an address with reverse geocoding
     reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
     # Request directions via public transit
@@ -24,3 +30,4 @@ if __name__ == "__main__":
                                                         regionCode='US',
                                                         locality='Mountain View', 
                                                         enableUspsCass=True)
+    '''
